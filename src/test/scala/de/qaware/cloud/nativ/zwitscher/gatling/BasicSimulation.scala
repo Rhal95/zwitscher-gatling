ba239a8992d75d41c55be4c121bc8bea14646f93
Lukas Weber <lukas.b.weber@student.hs-rm.de>
2019-02-05 17:13:43 +0100

    found good values for test

--- src/test/scala/de/qaware/cloud/nativ/zwitscher/gatling/BasicSimulation.scala
+++ src/test/scala/de/qaware/cloud/nativ/zwitscher/gatling/BasicSimulation.scala
@@ -6,40 +6,40 @@ import io.gatling.http.Predef._
 import scala.concurrent.duration.Duration
 
 class BasicSimulation extends Simulation {
-  val httpProtocol = http
+  private val httpProtocol = http
     .baseUrl("http://localhost:8081")
     .acceptHeader(" text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
     .acceptLanguageHeader("de,en-US;q=0.7,en;q=0.3")
     .acceptEncodingHeader("gzip, deflate")
     .userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.0")
-
-  val requestQuote = http("request_1")
+  private val requestQuote = http("requestQuotes")
     .get("/")
 
-  val requestTweets = http("request_2")
+  private val requestTweets = http("requestTweets")
     .get("/search?q=test")
 
-  val mediumDuration = Duration.create(5, "s")
-  val longDuration = Duration.create(30, "s")
+  private val shortDuration = Duration.create(5, "s")
+  private val mediumDuration = Duration.create(30, "s")
+  private val longDuration = Duration.create(5, "minutes")
 
   //opening webpage, wait, reload page
-  val scn1 = scenario("RequestQuotes")
+  private val scn1 = scenario("RequestQuotes")
     .exec(requestQuote)
-    .pause(mediumDuration)
+    .pause(shortDuration)
     .exec(requestQuote)
-    .pause(mediumDuration)
+    .pause(shortDuration)
 
   //opening webpage, searching for tweets, wait, search again
-  val scn2 = scenario("RequestTweets")
+  private val scn2 = scenario("RequestTweets")
       .exec(requestQuote)
+      .pause(shortDuration)
+      .exec(requestTweets)
       .pause(mediumDuration)
       .exec(requestTweets)
-      .pause(longDuration)
-      .exec(requestTweets)
-      .pause(longDuration)
+      .pause(mediumDuration)
 
-  setUp(scn1.inject(atOnceUsers(4)),
-    scn2.inject(atOnceUsers(4)))
+  setUp(scn1.inject(rampConcurrentUsers(1).to(2500).during(longDuration)),
+    scn2.inject(rampConcurrentUsers(1).to(2500).during(longDuration)))
     .protocols(httpProtocol)
 
 }
